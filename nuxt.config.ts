74d803a7fe9a2b87b61743aaaedf9fe235e6f8b0
Leonardo Nicola <leonardonicolares@gmail.com>
2023-02-22 15:27:57 -0300

    feat: add dependencies and create example store

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    
-})
+  typescript: {
+    shim: false,
+  },
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+});
